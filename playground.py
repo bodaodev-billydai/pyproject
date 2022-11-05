9235ae9627018585eb50b0e8ac4a030661d9351c
bodaodev-billydai <bodaodev-billydai@users.noreply.github.com>
2022-11-04 20:28:41 -0400

    testcase sample testcase1

--- playground.py
+++ playground.py
@@ -4,6 +4,8 @@ class PlayGround:
     def __init__(self):
         pass
     def run(self):
+        from testcases.testcase1 import testcase1
+        testcase1().run()
         pass
 
 playground = PlayGround()
---
+++ testcases/testcase1.py
@@ -0,0 +1,6 @@
+
+class testcase1:
+    def run(self):
+        print('this is a test case to print in console')
+
+__all__ = {'testcase1'}
